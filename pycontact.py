13854424c3ca80caf8d35401e67309cb7c149441
EnzoPinon <mrlorenzopinon@gmail.com>
2023-07-21 21:05:19 +0800

    add login and signup button

--- pycontact.py
+++ pycontact.py
@@ -4,10 +4,12 @@ from tkinter.ttk import Label, LabelFrame
 window = Tk()
 
 window.title("Pycontact-tracer: A contact tracing app in Python")
-window.geometry('1024x576')
+window.geometry('400x200')
 
 welcome_splash = LabelFrame(window, text='Welcome!')
 welcome_splash.pack(expand='yes', fill='both')
 first_label = Label(welcome_splash , text= "Please click to your destination.")
-first_label.place(x=100, y=200)
+first_label.place(x=110, y=30)
+employee_button = Button(welcome_splash, text="Sign up").place(x=160, y=120)
+admin_button = Button(welcome_splash, text='Log in').place(x=165, y=70)
 window.mainloop()
