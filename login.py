a54a99e84d4ce0a037b61cca3da1172707a96c9b
EnzoPinon <mrlorenzopinon@gmail.com>
2023-07-21 21:23:28 +0800

    create login attempt prompt

---
+++ login.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter.ttk import Label, LabelFrame
+
+class Login:
+    
+    def prompt():
+        login_window = Tk()
+        login_window.title("PyTracer Contact Tracing App")
+        login_window.geometry('400x400')
+
+        login_Frame = LabelFrame(login_window, text='Login to PyTracer')
+        login_Frame.pack(expand='yes', fill='both')
+        first_label = Label(login_Frame , text= "Welcome back!")
+        first_label.place(x=110, y=30)
+        second_label = Label(login_Frame, text="We're happy to see you again.")
+        second_label.place(x=110, y=50)
+        user_name = Label(login_Frame, text = "Username").place(x = 40, y = 60)
+        passcode = Label(login_Frame, text = "User Password").place(x = 40, y = 60)
+        user_name_input_area = Entry(login_Frame,width = 30).place(x = 110, y = 60) 
+        user_password_entry_area = Entry(login_Frame,width = 30).place(x = 110,y = 100)
+        login_window.mainloop()
--- pycontact.py
+++ pycontact.py
@@ -1,15 +1,16 @@
 from tkinter import *
 from tkinter.ttk import Label, LabelFrame
+from login import Login
 
 window = Tk()
 
-window.title("Pycontact-tracer: A contact tracing app in Python")
+window.title("PyTracer Contact Tracing App")
 window.geometry('400x200')
 
 welcome_splash = LabelFrame(window, text='Welcome!')
 welcome_splash.pack(expand='yes', fill='both')
 first_label = Label(welcome_splash , text= "Please click to your destination.")
 first_label.place(x=110, y=30)
-employee_button = Button(welcome_splash, text="Sign up").place(x=160, y=120)
-admin_button = Button(welcome_splash, text='Log in').place(x=165, y=70)
+signup = Button(welcome_splash, text="Sign up").place(x=160, y=120)
+login = Button(welcome_splash, text='Log in', command=Login.prompt()).place(x=165, y=70)
 window.mainloop()
