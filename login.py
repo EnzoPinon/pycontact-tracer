0f9e59a23bb587ab821664ae709d3d6f5510c14b
EnzoPinon <mrlorenzopinon@gmail.com>
2023-07-23 00:07:34 +0800

    Show/hide password now functional

--- login.py
+++ login.py
@@ -25,8 +25,19 @@ class Login:
         second_label.place(x=110, y=50)
         user_name = Label(login_Frame, text = "Username:").place(x = 40, y = 80)
         passcode = Label(login_Frame, text = "Password:",).place(x = 40, y = 120)
+
+        def password_show():
+            if user_password_entry_area.cget('show') == '':
+                user_password_entry_area.config(show= '*')
+                show_password.config(text='show')
+            else:
+                user_password_entry_area.config(show= '')
+                show_password.config(text='hide')
+        password_show = partial(password_show)
         user_name_input_area = Entry(login_Frame,width = 30).place(x = 110, y = 80) 
-        user_password_entry_area = Entry(login_Frame, show='*',width = 30).place(x = 110,y = 120)
+        user_password_entry_area = Entry(login_Frame, show='*',width = 30)
+        user_password_entry_area.place(x = 110,y = 120)
         go_back = Button(login_Frame, text="Go Back", command=return_prompt).place(x=40, y=150)
-        show_password = Button(login_Frame, text='show').place(x=310, y=117)
+        show_password = Button(login_Frame, text='show', command=password_show)
+        show_password.place(x=310, y=117)
         login_window.mainloop()
--- signup.py
+++ signup.py
@@ -20,17 +20,39 @@ class Signup:
         login_Frame = LabelFrame(login_window, text='Create an account')
         login_Frame.pack(expand='yes', fill='both')
         first_label = Label(login_Frame , text= "Get Started")
-        first_label.place(x=220, y=30)
+        first_label.place(x=215, y=30)
         second_label = Label(login_Frame, text="Part 01: account information")
         second_label.place(x=175, y=50)
         user_name = Label(login_Frame, text = "Username:").place(x = 40, y = 80)
         passcode = Label(login_Frame, text = "Your password:",).place(x = 40, y = 120)
         passcode = Label(login_Frame, text = "Confirm password:",).place(x = 40, y = 160)
         user_name_input_area = Entry(login_Frame,width = 30).place(x = 170, y = 80) 
-        user_password_entry_area = Entry(login_Frame, show='*',width = 30).place(x = 170,y = 120)
-        confirm_password_entry = Entry(login_Frame, show='*',width = 30).place(x = 170,y = 160)
+        user_password_entry_area = Entry(login_Frame, show='*',width = 30)
+        user_password_entry_area.place(x = 170,y = 120)
+        confirm_password_entry = Entry(login_Frame, show='*',width = 30)
+        confirm_password_entry.place(x = 170,y = 160)
         go_back = Button(login_Frame, text="Go Back", command=return_prompt).place(x=40, y=200)
+
+        def password_show():
+            if user_password_entry_area.cget('show') == '':
+                user_password_entry_area.config(show= '*')
+                show_password.config(text='show')
+            else:
+                user_password_entry_area.config(show= '')
+                show_password.config(text='hide')
         
-        show_password = Button(login_Frame, text='show').place(x=380, y=117)
-        show_password_again = Button(login_Frame, text='show').place(x=380, y=157)
+        def password_show_2():
+            if confirm_password_entry.cget('show') == '':
+                confirm_password_entry.config(show= '*')
+                show_password_again.config(text='show')
+            else:
+                confirm_password_entry.config(show= '')
+                show_password_again.config(text='hide')
+        password_show = partial(password_show)
+        password_show_2 = partial(password_show_2)
+
+        show_password = Button(login_Frame, text='show', command=password_show)
+        show_password.place(x=380, y=117)
+        show_password_again = Button(login_Frame, text='show', command=password_show_2)
+        show_password_again.place(x=380, y=157)
         login_window.mainloop()
