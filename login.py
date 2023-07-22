8476e4f0e2688abad11ecb2ae2f2cf3b2bca5708
EnzoPinon <mrlorenzopinon@gmail.com>
2023-07-23 02:47:57 +0800

    Created main menu temp and acc validation

--- login.py
+++ login.py
@@ -1,7 +1,11 @@
 from tkinter import *
+from tkinter import messagebox
 from tkinter.ttk import Label, LabelFrame
 from functools import partial
 from subprocess import call
+from pathlib import Path
+import csv
+import main_menu
 
 class Login:
     
@@ -34,10 +38,53 @@ class Login:
                 user_password_entry_area.config(show= '')
                 show_password.config(text='hide')
         password_show = partial(password_show)
-        user_name_input_area = Entry(login_Frame,width = 30).place(x = 110, y = 80) 
-        user_password_entry_area = Entry(login_Frame, show='*',width = 30)
+        
+        logname = StringVar()
+        logpass = StringVar()
+        user_name_input_area = Entry(login_Frame,width = 30, textvariable=logname)
+        user_name_input_area.place(x = 110, y = 80) 
+        user_password_entry_area = Entry(login_Frame, show='*',width = 30, textvariable=logpass)
         user_password_entry_area.place(x = 110,y = 120)
-        go_back = Button(login_Frame, text="Go Back", command=return_prompt).place(x=40, y=150)
+
+        def login_try(logname, logpass):
+            username = str(logname)
+            password = str(logpass)
+            login = Login.login_authenticator(username, password)
+            if login == True:
+                login_window.destroy()
+        login_try = partial(login_try, logname, logpass)
+
+        go_back = Button(login_Frame, text="Go Back", command=return_prompt).place(x=130, y=170)
+        go_auth = Button(login_Frame, text='Log in', command=login_try)
+        go_auth.place(x=200, y=170)
         show_password = Button(login_Frame, text='show', command=password_show)
         show_password.place(x=310, y=117)
         login_window.mainloop()
+    
+    def login_authenticator(username, password):
+        account_list = Path('./accounts.csv')
+        if account_list.is_file():
+            with open('accounts.csv', 'r') as user_list:
+                user_list_reader = csv.reader(user_list)
+                for column in user_list_reader:
+                    user_find_value = column[0]
+                    for row in user_find_value:
+                        if str(row) == username:
+                            user_login = row
+                            for column in user_login:
+                                recorded_password = str(column[1])
+                                if recorded_password != password:
+                                    messagebox.showerror('Incorrect password', 'Your password is invalid. Please try again.')
+                                    login = False
+                                    return login
+                                if recorded_password == password:
+                                    pass
+                            else:
+                                messagebox.showerror('User not found', 'The username is invalid. Please try again.')
+                                login = False
+                                return login
+        if account_list.is_file() == False:
+            open('accounts.csv', 'x')
+            messagebox.showinfo('Database created',"The database does not exist and has been created. Please make an account first!")
+            login = False
+            return login
---
+++ main_menu.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter.ttk import Label, LabelFrame
+from functools import partial
+from subprocess import call
+
+class main_menu:
+    pass
